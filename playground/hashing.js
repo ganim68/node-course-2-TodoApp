433fe2e0067e3fe810e4fb8e43c54d3b0cf43c69
Ganim <ganim68@gmail.com>
2018-04-11 18:49:46 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,24 @@
 const {SHA256}=require('crypto-js');
-const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-            id :10
-         };
-var token = jwt.sign(data,'123abc');
-console.log(`Token :${token}`);
-var decoded =jwt.verify(token,'123abc');
-console.log('Decoded :',decoded);
+ var password ='123abc!';
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//     console.log(hash);
+//     });
+// });
+
+var hashedpassword='$2a$10$Aij7TpakQd.AxTAyfrvkr./Q/Pv3OmDputFB.Dn48BnMrWVZHczri'
+bcrypt.compare(password,hashedpassword,(err,res)=>{
+    console.log(res);
+});
+// var data={
+//             id :10
+//          };
+// var token = jwt.sign(data,'123abc');
+// console.log(`Token :${token}`);
+// var decoded =jwt.verify(token,'123abc');
+// console.log('Decoded :',decoded);
 
 // var message ='I am user number 3';
 // var hash=SHA256(message).toString();
